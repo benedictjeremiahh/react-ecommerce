437a09e4a4a64137a06a9e562a993288f6a4ba2e
Benedict Jeremiah <benedict.jeremiah08@gmail.com>
2020-12-10 08:21:04 +0700

    add react router dom, update menu items

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
 import Homepage from "./pages/homepage/homepage.component";
+import { Switch, Route } from "react-router-dom";
 
 function App() {
 	return (
 		<div>
-			<Homepage />
+			<Switch>
+				<Route exact path="/" component={Homepage}></Route>
+			</Switch>
+			{/* <Homepage /> */}
 		</div>
 	);
 }
